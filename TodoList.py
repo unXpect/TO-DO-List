b49ddd79f8b2b8eea607b77c083f646d7e4582c6
unXpect <158562908+unXpect@users.noreply.github.com>
2024-02-03 13:02:47 +0300

    Update TodoList.py

--- TodoList.py
+++ TodoList.py
@@ -1,14 +1,15 @@
 from datetime import date
-f=open(f'{date.today()}.txt','w')
-f.write('-----TO-DO List-----' + '\n')
-while True:
-    b=input('Введите свои дела:')
-    if b.lower()=='stop':
-        print('Данные записаны в файл')
 
+
+f = open(f'{date.today()}.txt', 'w')
+f.write('-----TO-DO List-----' + '\n')
+
+while True:
+    b = input('Введите свои дела:')
+    if b.lower() == 'stop':
+        print('Данные записаны в файл')
         break
     else:
         f.write(b + '\n')
+        
 f.write('----------')
-
-
